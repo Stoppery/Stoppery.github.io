483760e5d22ebb29290196c63a3b9b9dae037c1b
Maxim <max.miromanov@yandex.ru>
2021-04-23 20:11:05 +0300

    first push

---
+++ javascript/hello_text.js
@@ -0,0 +1,19 @@
+var xmlhttp = new XMLHttpRequest();
+let words = [];
+
+
+xmlhttp.onreadystatechange = function(){
+	if(xmlhttp.status==200 && xmlhttp.readyState==4){    
+		words = xmlhttp.responseText.split('\n');
+		for (var i = 0; i <= words.length-1; i++){
+			(function(index) {
+				setTimeout(function() { document.getElementById("myh1id").innerHTML = words[index]; }, i*5000);
+			})(i);
+		}
+	}
+};
+
+
+xmlhttp.open("GET","../src/hello.txt",true);
+xmlhttp.send();
+
