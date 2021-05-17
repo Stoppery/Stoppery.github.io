47c0de530dbbc4a5fb13d00bfae467bca60ace0a
Stoppery <58211981+Stoppery@users.noreply.github.com>
2021-05-18 00:42:47 +0300

    try1

--- javascript/hello_text.js
+++ javascript/hello_text.js
@@ -1,18 +1,25 @@
 var xmlhttp = new XMLHttpRequest();
- let words = [];
+let words = [];
 
 
- xmlhttp.onreadystatechange = function(){
- 	if(xmlhttp.status==200 && xmlhttp.readyState==4){    
- 		words = xmlhttp.responseText.split('\n');
- 		for (var i = 0; i <= words.length-1; i++){
- 			(function(index) {
- 				setTimeout(function() { document.getElementById("myh1id").innerHTML = words[index]; }, i*5000);
- 			})(i);
- 		}
- 	}
- };
+xmlhttp.onreadystatechange = function(){
+	if(xmlhttp.status==200 && xmlhttp.readyState==4){    
+		words = xmlhttp.responseText.split('\n');
+		for (var i = 0; i <= words.length-1; i++){
+			(function(index) {
+				setTimeout(function() { document.getElementById("myh1id").innerHTML = words[index]; }, i*5000);
+			})(i);
+		}
+	}
+};
 
+const checkbox = document.querySelector(".theme_switcher");
 
- xmlhttp.open("GET","../src/hello.txt",true);
- xmlhttp.send();
+checkbox.addEventListener("change", function() {
+	const themeContainer = document.querySelector(".try");
+
+	themeContainer.classList.toggle('light');
+});
+
+xmlhttp.open("GET","/Stoppery.github.io/src/hello.txt",true);
+xmlhttp.send();
